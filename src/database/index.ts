96e48e09770c588ad7b43ec1f48fb3ea48c4fd0a
hsiqueira98 <henrisqc851271@gmail.com>
2026-04-21 13:52:27 -0300

    primeiro commit

---
+++ src/database/index.ts
@@ -0,0 +1,24 @@
+import { env } from "#env";
+import chalk from "chalk";
+import mongoose, { InferSchemaType, model } from "mongoose";
+import { guildSchema } from "./schemas/guild.js";
+import { memberSchema } from "./schemas/member.js";
+
+try {
+   console.log(chalk.blue("Connecting to MongoDB..."));
+   await mongoose.connect(env.MONGO_URI, { 
+      dbName: env.DATABASE_NAME || "database" 
+   });
+   console.log(chalk.green("MongoDB connected"));
+} catch(err){
+   console.error(err);
+   process.exit(1);
+}
+
+export const db = {
+   guilds: model("guild", guildSchema, "guilds"),
+   members: model("member", memberSchema, "members")
+};
+
+export type GuildSchema = InferSchemaType<typeof guildSchema>;
+export type MemberSchema = InferSchemaType<typeof memberSchema>;
